74d3ac6c67f9d89062ecf472544f9854525dbc2b
iradtaufique <iradukunda.ta@gmail.com>
2021-12-09 12:14:26 +0200

    make change in drf_ysg settings

--- api/urls.py
+++ api/urls.py
@@ -8,9 +8,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Fab|Waste API",
       default_version='v1',
-      description="Test description",
+      description="Fab Waste API",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
@@ -18,7 +18,6 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
-
 
 
 urlpatterns = [
@@ -30,6 +29,5 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
 
 ]
